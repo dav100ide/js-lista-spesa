f8fbc815f44701d5dc220e5518514b1e638e2d46
dav100ide <oreglio.dev@gmail.com>
2022-10-27 14:16:11 +0200

    prototipo ciclo while

---
+++ js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const shoppingList = [
+   'birra', // lista della spesa
+   'vodka',
+   'vino',
+   'tequila',
+   'rum',
+   'acqua',
+];
+
+let i = 0;
+while (i < shoppingList.length) {
+   console.log(i);
+   console.log(shoppingList[i]);
+   i++;
+}
