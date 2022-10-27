83765f50726ada5811d137d6aeace63f1fc5dc71
dav100ide <oreglio.dev@gmail.com>
2022-10-27 15:05:59 +0200

    js refactoring + css sfondo

--- js/main.js
+++ js/main.js
@@ -8,23 +8,9 @@ const shoppingList = [
    'rum',
    'acqua',
 ];
-// SOLUZIONE 1 OKAY 👌
-// const shoppingListHTML = document.getElementById('shopping-list');
-// let i = 0;
-// while (i < shoppingList.length) {
-//    const shoppingItem = shoppingList[i];
-//    const newLi = document.createElement('li');
-//    shoppingListHTML.append(newLi);
-//    newLi.innerHTML = shoppingItem;
-//    i++;
-// }
-
-// soluzione 2 non funziona 💩 non so cosa cambia🤷‍♂️
 const shoppingListHTML = document.getElementById('shopping-list');
 let i = 0;
 while (i < shoppingList.length) {
-   const shoppingItem = shoppingList[i];
-   const newLi = document.createElement('li');
-   shoppingListHTML.append((newLi.innerHTML = shoppingItem));
+   shoppingListHTML.innerHTML += `<li>${shoppingList[i]}</li>`;
    i++;
 }
